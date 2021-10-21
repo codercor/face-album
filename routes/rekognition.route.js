64b2afff487b5dcc3b23eb51aa5e901358e54ee1
Mustafa Çor <mmustafaa9@gmail.com>
2021-10-22 00:17:50 +0300

    Fix: folder added to searchfaces endpoint's body parameters

--- routes/rekognition.route.js
+++ routes/rekognition.route.js
@@ -5,9 +5,10 @@ const path = require('path');
 const fs = require('fs');
 router.post( "/searchFace",uploadSelfie.single("selfie"),async (req,res) => {
     const { file } = req;
+    const folder = req.body.folder
     let filePath = path.resolve(file.destination+"/"+file.filename);
     let selfie =  fs.readFileSync(filePath);
-    let results = await rekognitionService.searchPhotosBySelfie(selfie)
+    let results = await rekognitionService.searchPhotosBySelfie(selfie,folder);
     res.json(results);
     fs.rmSync(filePath)
 })
